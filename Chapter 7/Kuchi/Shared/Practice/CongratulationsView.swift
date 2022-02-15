5a4eda0462ada89b473e2e892f1d65a64783bcbe
mosariot <mosariot@gmail.com>
2022-02-15 23:10:28 +0300

    Chapter 7 completed

--- Chapter 7/Kuchi/Shared/Practice/CongratulationsView.swift
+++ Chapter 7/Kuchi/Shared/Practice/CongratulationsView.swift
@@ -33,6 +33,9 @@
 import SwiftUI
 
 struct CongratulationsView: View {
+  
+  @ObservedObject var challengesViewModel = ChallengesViewModel()
+  
   let avatarSize: CGFloat = 120
   let userName: String
   
@@ -41,7 +44,49 @@ struct CongratulationsView: View {
   }
   
   var body: some View {
-    EmptyView()
+    VStack {
+      Spacer()
+      Text("Congratulations!")
+        .font(.title)
+        .foregroundColor(.gray)
+      ZStack {
+        VStack(spacing: 0) {
+          Rectangle()
+            .frame(height: 90)
+            .foregroundColor(Color(red: 0.5, green: 0, blue: 0).opacity(0.2))
+          Rectangle()
+            .frame(height: 90)
+            .foregroundColor(Color(red: 0.6, green: 0.1, blue: 0.1).opacity(0.4))
+        }
+        Image(systemName: "person.fill")
+          .resizable()
+          .padding()
+          .frame(width: avatarSize, height: avatarSize)
+          .background(.white.opacity(0.5))
+          .cornerRadius(avatarSize / 2, antialiased: true)
+          .shadow(radius: 4)
+        VStack {
+          Spacer()
+          Text(userName)
+            .font(.largeTitle)
+            .foregroundColor(.white)
+            .fontWeight(.bold)
+            .shadow(radius: 7)
+        }
+        .padding()
+      }
+      .frame(height: 180)
+      Text("You're awesome!")
+        .fontWeight(.bold)
+        .foregroundColor(.gray)
+      Spacer()
+      Button(action: {
+        challengesViewModel.restart()
+      }, label: {
+        Text("Play Again")
+      })
+        .padding(.top)
+    }
   }
 }
 
